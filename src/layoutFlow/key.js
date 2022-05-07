24cc53dfb9184a355ebad18ffe83c3a96f12591a
freenokke <kosmatkov94@gmail.com>
2022-05-07 20:10:52 +0300

    feat: add animation when keys are clicked

--- src/layoutFlow/key.js
+++ src/layoutFlow/key.js
@@ -1,7 +1,7 @@
 export default class Key {
   constructor(parent, className, content, keyboard, isMode, tagName = 'button') {
     const el = document.createElement(tagName);
-    const [className1, className2] = className;
+    const [className1, className2] = className; // <== Деструктуризация
     el.className = `${className1} ${className2}`;
     el.innerHTML = content;
     this.keyboard = keyboard;
@@ -12,6 +12,15 @@ export default class Key {
         keyboard.print(this.content);
       });
     }
+    el.addEventListener('mousedown', () => {
+      el.classList.add('pressed');
+      el.addEventListener('mouseleave', () => {
+        el.classList.remove('pressed');
+      });
+    });
+    el.addEventListener('mouseup', () => {
+      el.classList.remove('pressed');
+    });
     this.node = el;
   }
 
